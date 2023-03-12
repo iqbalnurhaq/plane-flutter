ca564774cc3dd6012367873328d416ba5d504633
Iqbal Nur Haq <iqbalnur833@gmail.com>
2023-03-12 17:18:40 +0700

    Dynamic User When First SignUp

--- lib/ui/pages/bonus_page.dart
+++ lib/ui/pages/bonus_page.dart
@@ -1,5 +1,7 @@
 import 'package:airplane/ui/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../cubit/auth_cubit.dart';
 import '../../shared/theme.dart';
 
 class BonusPage extends StatelessWidget {
@@ -8,80 +10,88 @@ class BonusPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget bonusCard() {
-      return Container(
-        width: 300,
-        height: 211,
-        padding: EdgeInsets.all(defaultMargin),
-        decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/image_bonus_card.png'),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: kPrimaryColor.withOpacity(0.5),
-                blurRadius: 50,
-                offset: Offset(0, 10),
-              )
-            ]),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+      return BlocBuilder<AuthCubit, AuthState>(
+        builder: (context, state) {
+          if (state is AuthSuccess) {
+            return Container(
+              width: 300,
+              height: 211,
+              padding: EdgeInsets.all(defaultMargin),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/image_bonus_card.png'),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: kPrimaryColor.withOpacity(0.5),
+                      blurRadius: 50,
+                      offset: Offset(0, 10),
+                    )
+                  ]),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
                     children: [
-                      Text(
-                        'Name',
-                        style: whiteTextStyle.copyWith(fontWeight: light),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Name',
+                              style: whiteTextStyle.copyWith(fontWeight: light),
+                            ),
+                            Text(
+                              state.user.name,
+                              style: whiteTextStyle.copyWith(
+                                  fontSize: 20, fontWeight: medium),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 24,
+                        height: 24,
+                        margin: EdgeInsets.only(right: 6),
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("assets/logo.png"),
+                          ),
+                        ),
                       ),
                       Text(
-                        'Iqbal Nur Haq',
+                        'Pay',
                         style: whiteTextStyle.copyWith(
-                            fontSize: 20, fontWeight: medium),
-                        overflow: TextOverflow.ellipsis,
-                      ),
+                          fontSize: 16,
+                          fontWeight: medium,
+                        ),
+                      )
                     ],
                   ),
-                ),
-                Container(
-                  width: 24,
-                  height: 24,
-                  margin: EdgeInsets.only(right: 6),
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage("assets/logo.png"),
+                  SizedBox(
+                    height: 41,
+                  ),
+                  Text(
+                    'Balance',
+                    style: whiteTextStyle.copyWith(
+                      fontWeight: light,
                     ),
                   ),
-                ),
-                Text(
-                  'Pay',
-                  style: whiteTextStyle.copyWith(
-                    fontSize: 16,
-                    fontWeight: medium,
-                  ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 41,
-            ),
-            Text(
-              'Balance',
-              style: whiteTextStyle.copyWith(
-                fontWeight: light,
+                  Text(
+                    'IDR 200.000.000',
+                    style: whiteTextStyle.copyWith(
+                      fontSize: 26,
+                      fontWeight: medium,
+                    ),
+                  )
+                ],
               ),
-            ),
-            Text(
-              'IDR 200.000.000',
-              style: whiteTextStyle.copyWith(
-                fontSize: 26,
-                fontWeight: medium,
-              ),
-            )
-          ],
-        ),
+            );
+          } else {
+            return SizedBox();
+          }
+        },
       );
     }
 
